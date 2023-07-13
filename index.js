375ba46991c7c1cbd80f60610252ce2f35b0d2e3
Choirandvice <ryan.thomas.harrison@gmail.com>
2023-07-13 12:15:02 +0930

    3.6 complete

--- index.js
+++ index.js
@@ -84,6 +84,12 @@ app.post('/api/persons', (request,response) => {
     })
   }
 
+  if (persons.some(person=>person.name.toLowerCase()===body.name.toLowerCase())){
+    return response.status(400).json({ 
+      error: 'name already in phonebook' 
+    })
+  }
+
   const newPerson = {
     id: Math.floor(Math.random()*1000000),
     name: body.name,
