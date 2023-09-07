af04905d206d83cc9e0d8129442760a4f3dc7a7f
Choirandvice <ryan.thomas.harrison@gmail.com>
2023-09-07 21:31:29 +0930

    updated error handling and validation

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
+class AlreadyDeletedError extends Error{
+  constructor(message){
+    super(message)
+    this.name = "AlreadyDeletedError"
+  }
+}
 
 morgan.token('body', (request,response)=>{
   return JSON.stringify(request.body)
@@ -22,8 +28,6 @@ const respondWithTiny = (request, response) => {
 const respondWithBody = (request,response) => {
   return (request.method==="POST"||request.method==="PUT")
 }
-
-// app.use(morgan((tokens, request, response)=>{
 app.use(morgan('tiny',{ skip: respondWithBody }))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {skip: respondWithTiny}))
@@ -36,7 +40,6 @@ app.get('/api/persons', (request,response) => {
     response.json(persons)
   })
 })
-
 
 app.get('/api/persons/:id', (request, response, next) => {
 
@@ -83,13 +86,7 @@ app.post('/api/persons', (request,response,next) => {
 
   const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'person needs a name and a number' 
-    })
-  }
-
-  console.log(`creating person: ${body.name}|${body.number}`)
+  console.log(`creating person: ${request.body}`)
 
   const newPerson = new Person({
     name: body.name,
@@ -99,11 +96,13 @@ app.post('/api/persons', (request,response,next) => {
   newPerson.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 
 })
 
 
 app.put('/api/persons/:id', (request, response, next) => {
+
   const body = request.body
   
   const person = {
@@ -111,9 +110,17 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+  console.log(`updating person: ${body.name}|${body.number}`)
+
+  Person.findByIdAndUpdate(request.params.id, person, {new:true, runValidators: true, context: 'query'})
     .then(updatedPerson => {
-      response.json(updatedPerson)
+      // this returns null if the person has already been removed
+      if(updatedPerson!=null){
+        response.json(updatedPerson)
+      }
+      else{
+        throw new AlreadyDeletedError(`Error: ${body.name} already removed from the server`)
+      }
     })
     .catch(error => next(error))
 })
@@ -130,10 +137,16 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error("error:", error)
+  console.error("error.name", error.name)
+  console.error("Errorhandler:",error.message)
 
   if(error.name === 'CastError'){
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message})
+  } else if (error.name === 'AlreadyDeletedError'){
+    return response.status(500).json({error: error.message})
   }
 
   next(error)
